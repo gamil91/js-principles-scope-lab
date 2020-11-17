e68120edd26472f9a2c4a359db0b1ce31662b783
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-16 19:03:28 -0800

    scopes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName =  "bob";
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "Still not Bob"
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "This should not be able to change"
+}
